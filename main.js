3856dc7a1c1e950bdef85d02aeab3d16c28e3357
Victor Velasquez <victorv24@gmail.com>
2018-07-13 11:21:24 -0700

    added specials API

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
 $(function () {
-	
+
+	var specialsAPI = 'https://obscure-tundra-54269.herokuapp.com/bar-food'
+	$.getJSON(specialsAPI, function(data) {
+		console.log('I have recieved the specials, sir!')
+		console.log(data.appetizers)
+	})
+
 })
