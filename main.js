d6f04309a7e866aace19424c5afa8548dcae851f
Keen <austin.kuehne@gmail.com>
2018-07-13 10:14:34 -0700

    commit

--- main.js
+++ main.js
@@ -1,21 +1,27 @@
-<<<<<<< HEAD
+
 $(document).ready(function() {
 	
-	$("#tabs li").on('click', function() {
-		
-		$("#tabs li .nav").removeClass('show')
-		$(this).find('.nav').addClass('show')
-	})
+	$('.tab-panels .tabs li').on('click', function() {
 
+		 var $panel = $(this).closest('.tab-panels')
 
+        $panel.find('.tabs li.active').removeClass('active')
+        $(this).addClass('active')
 
-=======
-$(function () {
-	$("#calendar").datepicker()formatDate( "DD, MM d, yy", new Date( 2007, 7 - 1, 14 ), {
-  dayNamesShort: $.datepicker.regional[ "fr" ].dayNamesShort,
-  dayNames: $.datepicker.regional[ "fr" ].dayNames,
-  monthNamesShort: $.datepicker.regional[ "fr" ].monthNamesShort,
-  monthNames: $.datepicker.regional[ "fr" ].monthNames
-});
->>>>>>> c32a30371d8a22413d78cc0100a99ec8455775f2
+        var panelToShow = $(this).attr('rel')
+
+        $panel.find('.panel.active').slideUp(650, showNextPanel)
+
+        function showNextPanel() {
+            $(this).removeClass('active')
+
+            $('#'+panelToShow).slideDown(650, function() {
+                $(this).addClass('active')
+            })
+        }
+
+	})	
 })
+
+
+
