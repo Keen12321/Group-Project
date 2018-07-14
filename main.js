2eade2fa500ae1ca51fd457d3066c3a051c617f6
Victor Velasquez <victorv24@gmail.com>
2018-07-14 15:22:42 -0700

    minor changes to code

--- main.js
+++ main.js
@@ -5,37 +5,30 @@
  */
 
 $(function () {
+
 // Daily Special API
-	var specialsAPI = 'https://obscure-tundra-54269.herokuapp.com/bar-food'
-	$.getJSON(specialsAPI, function(specials) {
+	var specialsJSON = 'https://obscure-tundra-54269.herokuapp.com/bar-food'
+
+	$.getJSON(specialsJSON, function(specials) {
+
 		// $('#info-cards__food-photo').text(data.appetizers[0].image) // TO DO: add photos to be pulled depending on date @victorinspce
+
 		$('#info-cards__food-item').text(specials.appetizers[0].name)
+
 		$('#info-cards__food-description').text(specials.appetizers[0].description)
+
 	})
 
 	// Menu API
   var menuJSON = 'https://obscure-tundra-54269.herokuapp.com/casual-dining'
+	
 	$.getJSON (menuJSON, function(menu) {
 
 		$('#menuname').text(menu.name)
+
 		$('#menuprice').text(menu.price)
+
 		$('#menudescription').text(menu.description)
-
-		// const name = document.getElementById('menuname')
-		// const price = document.getElementById('menuprice')
-		// const description = document.getElementById('menudescription')
-
-		// menu.appetizers.forEach(function(data) {
-		// 	name.append(data.name)
-		// })
-
-		// menu.appetizers.forEach(function(data) {
-		// 	price.append(data.price)
-		// })
-
-		// menu.appetizers.forEach(function(data) {
-		// 	description.append(data.description)
-		// })
 	})
 
 // Hero Image Carousel
@@ -48,23 +41,29 @@ $(function () {
 
 // Tabs Menu Section
 	$('.tab-panels .tabs li').on('click', function() {
-		
-		var $panel = $(this).closest('.tab-panels')
 
-		$panel.find('.tabs li.active').removeClass('active')
+		var panel = $(this).closest('.tab-panels')
+
+		panel.find('.tabs li.active').removeClass('active')
+		
 		$(this).addClass('active')
 
-		$panel.find('panel.active').slideUp(650, panelToShow)
+		panel.find('panel.active').slideUp(650, panelToShow)
 
 		var panelToShow = $(this).attr('rel')
 
 		function showNextPanel() {
+			
 			$(this).removeClass('active')
 
 			$('#'+panelToShow).slideDown(650, function() {
+				
 				$(this).addClass('active')
+			
 			})
+		
 		}
+	
 	})
 
 })
