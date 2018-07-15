c6ee77371b5fdfa8a700bf54cdc41277e83b145f
Mike Michaud <michaud.michael@gmail.com>
2018-07-14 17:22:04 -0700

    cleaned up code, 66pct responsive to 700px

--- main.js
+++ main.js
@@ -1,22 +1,16 @@
-$(function () {
-
-	var specialsAPI = 'https://obscure-tundra-54269.herokuapp.com/bar-food'
-	$.getJSON(specialsAPI, function(data) {
-		// $('#info-cards__food-photo').text(data.appetizers[0].image)
-		$('#info-cards__food-item').text(data.appetizers[0].name)
-		$('#info-cards__food-description').text(data.appetizers[0].description)
-	})
+$(document).ready(function() {
 
     $(".hero img:gt(0)").hide()
     setInterval(function(){
       $(".hero :first-child").fadeOut()
       .next("img").fadeIn()
       .end().appendTo(".hero")
-    }, 5000)
+    }, 3000)
 
-	$('.tab-panels .tabs li').on('click', function() {
+  
+  $('.tab-panels .tabs li').on('click', function() {
 
-		 var $panel = $(this).closest('.tab-panels')
+     var $panel = $(this).closest('.tab-panels')
 
         $panel.find('.tabs li.active').removeClass('active')
         $(this).addClass('active')
@@ -25,15 +19,42 @@ $(function () {
 
 
         function showNextPanel() {
-          $(this).removeClass('active')
+            $(this).removeClass('active')
 
+            $('#'+panelToShow).slideDown(function() {
+                $(this).addClass('active')
+            })
+        }
+        $panel.find('.panel.active').slideUp(showNextPanel)
 
-          $('#'+panelToShow).slideDown(650, function() {
-          $(this).addClass('active')
-        })
-      }
-	})
+  })
 
+  $("#nav1").on('click', function(){
+               // if ($('.tabs li.active') == true) {
+               $('#panel2').removeClass('active').css("display", "none")
+               $('#panel3').removeClass('active').css("display", "none")
+             $("#panel1").addClass('active').css("display", "inline-block")
+              console.log('message here', $('.tabs li.active'))
+             })
+ 
+ 
+ 
+  $("#nav2").on('click', function(){
+             // if ($('.tabs li.active') == true) {
+               $('#panel1').removeClass('active').css("display", "none")
+               $('#panel3').removeClass('active').css("display", "none")
+             $("#panel2").addClass('active').css("display", "inline-block")
+              console.log('another message', $('.tabs li.active'))
+             })
+ 
+ 
+     
+  $("#nav3").on('click', function(){
+               // if ($('.tabs li.active') == true) {
+               $('#panel1').removeClass('active').css("display", "none")
+               $('#panel2').removeClass('active').css("display", "none")
+             $("#panel3").addClass('active').css("display", "inline-block")
+             })
 
     var menuJSON = 'https://obscure-tundra-54269.herokuapp.com/casual-dining'
     $.getJSON (menuJSON, function(menu) {
@@ -53,5 +74,18 @@ $(function () {
             description.append(data.description)
         })
     })
+    // Daily Special API
+  var specialsJSON = 'https://obscure-tundra-54269.herokuapp.com/bar-food'
+
+  $.getJSON(specialsJSON, function(specials) {
+
+    // $('#info-cards__food-photo').text(data.appetizers[0].image) // TO DO: add photos to be pulled depending on date @victorinspce
+
+    $('#info-cards__food-item').text(specials.appetizers[0].name)
+
+    $('#info-cards__food-description').text(specials.appetizers[0].description)
+
+  })
 
 })
+
