7fdcb26c53036cac737dff2e3c71b33e1d3a429c
Keen <austin.kuehne@gmail.com>
2018-07-14 21:17:49 -0700

    almost done

--- main.js
+++ main.js
@@ -8,10 +8,21 @@ $(document).ready(function() {
       .end().appendTo(".hero")
     }, 3000)
 
-	
-	$('.tab-panels .tabs li').on('click', function() {
+        var specialsJSON = 'https://obscure-tundra-54269.herokuapp.com/bar-food'
 
-		 var $panel = $(this).closest('.tab-panels')
+    $.getJSON(specialsJSON, function(specials) {
+
+        // $('#info-cards__food-photo').text(data.appetizers[0].image) // TO DO: add photos to be pulled depending on date @victorinspce
+
+        $('#info-cards__food-item').text(specials.appetizers[0].name)
+
+        $('#info-cards__food-description').text(specials.appetizers[0].description)
+
+    })
+
+    $('.tab-panels .tabs li').on('click', function() {
+
+         var $panel = $(this).closest('.tab-panels')
 
         $panel.find('.tabs li.active').removeClass('active')
         $(this).addClass('active')
@@ -28,25 +39,74 @@ $(document).ready(function() {
         }
         $panel.find('.panel.active').slideUp(showNextPanel)
 
-	})
+    })
 
     var menuJSON = 'https://obscure-tundra-54269.herokuapp.com/casual-dining'
     $.getJSON (menuJSON, function(menu) {
-        const name = document.getElementById('menuname')
-        const price = document.getElementById('menuprice')
-        const description = document.getElementById('menudescription')
+        // const name = document.getElementById('menuname')
+        // const price = document.getElementById('menuprice')
+        // const description = document.getElementById('menudescription')
 
-        menu.appetizers.forEach(function(data) {
-            name.append(data.name)
-        })
+        // menu.appetizers.forEach(function(data) {
+        //     name.append(data.name)
+        // })
 
-        menu.appetizers.forEach(function(data) {
-            price.append(data.price)
-        })
+        // menu.appetizers.forEach(function(data) {
+        //     price.append(data.price)
+        // })
 
-        menu.appetizers.forEach(function(data) {
-            description.append(data.description)
-        })
+        // menu.appetizers.forEach(function(data) {
+        //     description.append(data.description)
+        // })
+        $('#menuname1').text(menu.appetizers[0].name)
+        $('#menuprice1').text(menu.appetizers[0].price) 
+        $('#menudescription1').text(menu.appetizers[0].description)         
+
+        $('#menuname2').text(menu.appetizers[1].name)
+        $('#menuprice2').text(menu.appetizers[1].price)
+        $('#menudescription2').text(menu.appetizers[1].description)
+
+        $('#menuname3').text(menu.appetizers[2].name)
+        $('#menuprice3').text(menu.appetizers[2].price) 
+        $('#menudescription3').text(menu.appetizers[2].description)         
+
+        $('#menuname4').text(menu.appetizers[3].name)
+        $('#menuprice4').text(menu.appetizers[3].price)
+        $('#menudescription4').text(menu.appetizers[3].description)
+
+
+        $('#entreename1').text(menu.entrees[0].name)
+        $('#entreeprice1').text(menu.entrees[0].price)
+        $('#entreedescription1').text(menu.entrees[0].description)
+
+        $('#entreename2').text(menu.entrees[1].name)
+        $('#entreeprice2').text(menu.entrees[1].price)
+        $('#entreedescription2').text(menu.entrees[1].description)
+
+        $('#entreename3').text(menu.entrees[2].name)
+        $('#entreeprice3').text(menu.entrees[2].price)
+        $('#entreedescription3').text(menu.entrees[2].description)
+
+        $('#entreename3').text(menu.entrees[3].name)
+        $('#entreeprice3').text(menu.entrees[3].price)
+        $('#entreedescription3').text(menu.entrees[3].description)
+
+
+        $('#dessertname1').text(menu.desserts[0].name)
+        $('#dessertprice1').text(menu.desserts[0].price)
+        $('#dessertdescription1').text(menu.desserts[0].description)
+
+        $('#dessertname2').text(menu.desserts[1].name)
+        $('#dessertprice2').text(menu.desserts[1].price)
+        $('#dessertdescription2').text(menu.desserts[1].description)
+
+        $('#dessertname3').text(menu.desserts[2].name)
+        $('#dessertprice3').text(menu.desserts[2].price)
+        $('#dessertdescription3').text(menu.desserts[2].description)
+
+
+
+
     })
 
 })
